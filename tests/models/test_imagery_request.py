c23195c0ccaf5626fd2fe3a1ab243428ff3b16c7
Amy Boyer <amy@zooniverse.org>
2019-04-19 10:53:10 -0700

    test the serializer method for imagery requests

--- tests/models/test_imagery_request.py
+++ tests/models/test_imagery_request.py
@@ -1,14 +1,13 @@
 from django.test import TestCase
 from api.models import ImageryRequest
+from datetime import datetime
 
 import sys
 sys.dont_write_bytecode = True
 
 class TestImageryRequest(TestCase):
     def setUp(self):
-        self.test_request = ImageryRequest()
-        self.test_request.dataset_name = 'LANDSAT_8'
+        self.test_request = ImageryRequest(dataset_name='LANDSAT_8', project_id=123, created_at=datetime.fromisoformat('2019-05-17'))
 
-    def test_failing(self):
-        "it tests stuff"
-        assert self.test_request.dataset_name == 'LANDSAT_8'
+    def test___str__(self):
+        assert '[ImageryRequest project 123 at 2019-05-17]' == self.test_request.__str__()
