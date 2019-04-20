3c4d04927cc9aed951ad99fbc842a633670ff770
Amy Boyer <amy@zooniverse.org>
2019-04-20 14:30:28 -0700

    adds dataset name to search

--- tests/models/test_imagery_search.py
+++ tests/models/test_imagery_search.py
@@ -7,18 +7,28 @@ class TestImagerySearch(TestCase):
         filter = ImagerySearch.value_filter('WRS Path', 5)
         assert filter == { 'filterType': 'value', 'fieldId': 20514, 'value': 5 }
 
+    def test_builds_search_from_dataset_name(self):
+        search = ImagerySearch.add_dataset_name({}, "L8")
+        assert json.dumps(search) == '{"datasetName": "L8"}'
+
+        search = ImagerySearch.add_dataset_name({'datasetName': 'L7'}, "L8")
+        assert json.dumps(search) == '{"datasetName": "L8"}'
+
+        search = ImagerySearch.add_dataset_name({'foo': 'bar'}, "L8")
+        assert json.dumps(search) == '{"foo": "bar", "datasetName": "L8"}'
+
     def test_builds_search_from_row_and_path(self):
         search = ImagerySearch.add_wgs_row_and_path({}, 1, 10)
-        assert json.dumps(search) == '{"additional_criteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
+        assert json.dumps(search) == '{"additionalCriteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
 
         search = ImagerySearch.add_wgs_row_and_path({'foo':'bar'}, 1, 10)
-        assert json.dumps(search) == '{"foo": "bar", "additional_criteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
+        assert json.dumps(search) == '{"foo": "bar", "additionalCriteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
 
-        search = ImagerySearch.add_wgs_row_and_path({'additional_criteria': {}}, 1, 10)
-        assert json.dumps(search) == '{"additional_criteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
+        search = ImagerySearch.add_wgs_row_and_path({'additionalCriteria': {}}, 1, 10)
+        assert json.dumps(search) == '{"additionalCriteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
 
-        search = ImagerySearch.add_wgs_row_and_path({'additional_criteria': {'filterType': 'and', 'childFilters': []}}, 1, 10)
-        assert json.dumps(search) == '{"additional_criteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
+        search = ImagerySearch.add_wgs_row_and_path({'additionalCriteria': {'filterType': 'and', 'childFilters': []}}, 1, 10)
+        assert json.dumps(search) == '{"additionalCriteria": {"filterType": "and", "childFilters": [{"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
 
-        search = ImagerySearch.add_wgs_row_and_path({'additional_criteria': {'filterType': 'and', 'childFilters': ['blah']}}, 1, 10)
-        assert json.dumps(search) == '{"additional_criteria": {"filterType": "and", "childFilters": ["blah", {"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
+        search = ImagerySearch.add_wgs_row_and_path({'additionalCriteria': {'filterType': 'and', 'childFilters': ['blah']}}, 1, 10)
+        assert json.dumps(search) == '{"additionalCriteria": {"filterType": "and", "childFilters": ["blah", {"filterType": "value", "fieldId": 20514, "value": 10}, {"filterType": "value", "fieldId": 20516, "value": 1}]}}'
--- theia/api/models/imagery_search.py
+++ theia/api/models/imagery_search.py
@@ -1,6 +1,3 @@
-from .imagery_request import ImageryRequest
-import json
-
 class ImagerySearch:
     additional_field_ids = {
         "WRS Path": 20514,
@@ -10,16 +7,22 @@ class ImagerySearch:
     def __init__(imagery_request):
         return self
 
-    def build_search(self):
-        return ''
+    @classmethod
+    def build_search(cls, imagery_request):
+        return {}
+
+    @classmethod
+    def add_dataset_name(cls, search, name):
+        search['datasetName'] = name
+        return search
 
     @classmethod
     def add_wgs_row_and_path(cls, search, row, path):
-        search.setdefault('additional_criteria', {})
-        search['additional_criteria'].setdefault('filterType', 'and')
-        search['additional_criteria'].setdefault('childFilters', [])
+        search.setdefault('additionalCriteria', {})
+        search['additionalCriteria'].setdefault('filterType', 'and')
+        search['additionalCriteria'].setdefault('childFilters', [])
 
-        filters = search['additional_criteria']['childFilters']
+        filters = search['additionalCriteria']['childFilters']
         filters.append(cls.value_filter('WRS Path', path))
         filters.append(cls.value_filter('WRS Row', row))
         return search
