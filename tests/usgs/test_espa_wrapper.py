aeb2b59670109bb6ec34c5c63840fc9f2ea90ec8
Amy Boyer <amy@zooniverse.org>
2019-04-22 07:38:02 -0700

    first steps

--- tests/usgs/test_espa_wrapper.py
+++ tests/usgs/test_espa_wrapper.py
@@ -1,8 +1,47 @@
 from usgs import EspaWrapper
 from unittest import mock
+from requests.auth import HTTPBasicAuth
 
 
 class TestEspaWrapper:
     def test_api_url(self):
         assert EspaWrapper.api_url('foo') == 'https://espa.cr.usgs.gov/api/v1/foo'
         assert EspaWrapper.api_url('') == 'https://espa.cr.usgs.gov/api/v1/'
+
+    def test_espa_credentials(self):
+        assert EspaWrapper.espa_credentials(username='u', password='p') == HTTPBasicAuth('u','p')
+
+    def test_espa_prepare(self):
+        with mock.patch('usgs.EspaWrapper.espa_credentials') as mockCredentials:
+            mockCredentials.return_value = {'username': 'password'}
+            result = EspaWrapper.espa_prepare(None)
+
+            assert 'headers' in result
+            assert 'auth' in result
+            assert result['headers'] == {'Content-Type': 'application/json'}
+            assert result['auth'] == {'username': 'password'}
+
+        with mock.patch('usgs.EspaWrapper.espa_credentials') as mockCredentials:
+            mockCredentials.return_value = {'username': 'password'}
+            result = EspaWrapper.espa_prepare(None, headers={'X-Foo': 'bar'})
+
+            assert 'headers' in result
+            assert 'auth' in result
+            assert result['headers'] == {'Content-Type': 'application/json', 'X-Foo': 'bar'}
+            assert result['auth'] == {'username': 'password'}
+
+    def test_espa_post(self):
+        with mock.patch('requests.post') as mockPost, \
+                mock.patch('usgs.EspaWrapper.espa_prepare') as mockPrepare:
+            mockPrepare.return_value = {}
+
+            EspaWrapper.espa_post('', None)
+            mockPost.assert_called_once_with(EspaWrapper.api_url(''))
+
+            mockPost.reset_mock()
+            EspaWrapper.espa_post('', {'foo': 'bar'})
+            mockPost.assert_called_once_with(EspaWrapper.api_url(''))
+
+            mockPost.reset_mock()
+            EspaWrapper.espa_post('', {'foo': 'bar'}, headers={'X-Foo': 'bar'})
+            mockPost.assert_called_once_with(EspaWrapper.api_url(''))
--- theia/usgs/espa_wrapper.py
+++ theia/usgs/espa_wrapper.py
@@ -1,9 +1,38 @@
 from urllib.parse import urljoin
+from requests.auth import HTTPBasicAuth
+import requests
+from os import environ
+
+
 class EspaWrapper:
     @classmethod
     def api_url(cls, path):
         # return urljoin('https://demo1580318.mockable.io/', path)
         return urljoin('https://espa.cr.usgs.gov/api/v1/', path)
 
-    def foo():
-        return 'bar'
+    @classmethod
+    def espa_prepare(cls, request_data, **kwargs):
+        headers = {'Content-Type': 'application/json'}
+        if 'headers' in kwargs:
+            headers = {**headers, **(kwargs['headers'])}
+
+        auth = cls.espa_credentials()
+
+        new_args = {
+            'headers': headers,
+            'auth': auth
+        }
+        return {**kwargs, **new_args}
+
+    @classmethod
+    def espa_post(cls, url, request_data, **kwargs):
+        new_args = cls.espa_prepare(request_data, **kwargs)
+        return requests.post(cls.api_url(url), **new_args).json()
+
+    @classmethod
+    def list_orders(cls):
+        return
+
+    @classmethod
+    def espa_credentials(cls, username=environ['USGS_USERNAME'], password=['USGS_PASSWORD']):
+        return HTTPBasicAuth(username, password)
