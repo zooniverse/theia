197f08266d107eda6c95addeba5412adcead9545
Amy Boyer <amyrebecca@users.noreply.github.com>
2019-04-22 10:16:09 -0700

    wrap the ESPA api for ordering imagery (#30)
    * add test file and class file
    
    * first steps
    
    * better test
    
    * list-orders method
    
    * fix
    
    * handle request data if it's present
    
    * order status check
    
    * order products
    
    * enumerate available products
    
    * utility method to order all products
    
    * retrieve download urls from espa server
    
    * espa wrapper complete
    
    * order first matching scene

---
+++ tests/usgs/test_espa_wrapper.py
@@ -0,0 +1,157 @@
+from usgs import EspaWrapper
+from unittest import mock
+from requests.auth import HTTPBasicAuth
+
+
+class TestEspaWrapper:
+    def test_api_url(self):
+        assert EspaWrapper.api_url('foo') == 'https://espa.cr.usgs.gov/api/v1/foo'
+        assert EspaWrapper.api_url('') == 'https://espa.cr.usgs.gov/api/v1/'
+
+    def test_espa_credentials(self):
+        assert EspaWrapper.espa_credentials(username='u', password='p') == HTTPBasicAuth('u','p')
+
+    def test_espa_prepare(self):
+        with mock.patch('usgs.EspaWrapper.espa_credentials') as mockCredentials:
+            mockCredentials.return_value = {'username': 'password'}
+            result = EspaWrapper.espa_prepare(None)
+
+            assert 'headers' in result
+            assert 'auth' in result
+            assert result['headers'] == {'Content-Type': 'application/json'}
+            assert result['auth'] == {'username': 'password'}
+
+        with mock.patch('usgs.EspaWrapper.espa_credentials') as mockCredentials:
+            mockCredentials.return_value = {'username': 'password'}
+            result = EspaWrapper.espa_prepare(None, headers={'X-Foo': 'bar'})
+
+            assert 'headers' in result
+            assert 'auth' in result
+            assert result['headers'] == {'Content-Type': 'application/json', 'X-Foo': 'bar'}
+            assert result['auth'] == {'username': 'password'}
+
+    def test_espa_post(self):
+        with mock.patch('requests.post') as mockPost, \
+                mock.patch('usgs.EspaWrapper.espa_prepare') as mockPrepare:
+            mockPrepare.return_value = {}
+
+            EspaWrapper.espa_post('', None)
+            mockPost.assert_called_once_with(EspaWrapper.api_url(''))
+
+            mockPost.reset_mock()
+            EspaWrapper.espa_post('', {'foo': 'bar'})
+            mockPost.assert_called_once_with(EspaWrapper.api_url(''), json={'foo': 'bar'})
+
+            mockPost.reset_mock()
+            EspaWrapper.espa_post('', {'foo': 'bar'}, headers={'X-Foo': 'bar'})
+            mockPost.assert_called_once_with(EspaWrapper.api_url(''), json={'foo': 'bar'})
+
+            mockPrepare.return_value = {'foo': 'bar'}
+            mockPost.reset_mock()
+            EspaWrapper.espa_post('foo', None)
+            mockPost.assert_called_once_with(EspaWrapper.api_url('foo'), foo='bar')
+
+    def test_espa_get(self):
+        with mock.patch('requests.get') as mockGet, \
+                mock.patch('usgs.EspaWrapper.espa_prepare') as mockPrepare:
+            mockPrepare.return_value = {}
+
+            EspaWrapper.espa_get('', None)
+            mockGet.assert_called_once_with(EspaWrapper.api_url(''))
+
+            mockGet.reset_mock()
+            EspaWrapper.espa_get('', 'payload')
+            mockGet.assert_called_once_with(EspaWrapper.api_url('')+'payload')
+
+            mockGet.reset_mock()
+            EspaWrapper.espa_get('', 'payload', headers={'X-Foo': 'bar'})
+            mockGet.assert_called_once_with(EspaWrapper.api_url('')+'payload')
+
+            mockPrepare.return_value = {'foo': 'bar'}
+            mockGet.reset_mock()
+            EspaWrapper.espa_get('foo', None)
+            mockGet.assert_called_once_with(EspaWrapper.api_url('foo'), foo='bar')
+
+    def test_list_orders(self):
+        with mock.patch('usgs.EspaWrapper.espa_get') as mockGet:
+            mockGet.return_value = ['orderid_1', 'orderid_2']
+
+            EspaWrapper.list_orders()
+            mockGet.assert_called_once_with('list-orders', None)
+
+    def test_order_status(self):
+        with mock.patch('usgs.EspaWrapper.espa_get') as mockGet:
+            mockGet.return_value = {'foo': 'bar', 'status': 'purged'}
+
+            status = EspaWrapper.order_status('1234')
+            mockGet.assert_called_once_with('order-status', '1234')
+            assert status == 'purged'
+
+    def test_order(self):
+        with mock.patch('usgs.EspaWrapper.espa_post') as mockPost:
+            mockPost.return_value = {'orderid': 'aa1234', 'status': 'ordered'}
+
+            orderid = EspaWrapper.order('olitirs8_collection','LC08-1234', 'sr')
+            mockPost.assert_called_once_with(
+                'order',
+                {
+                    'olitirs8_collection': {
+                        'inputs': ['LC08-1234'],
+                        'products': ['sr']
+                    },
+                    'format': 'gtiff'
+                }
+            )
+
+            assert orderid == 'aa1234'
+
+    def test_available_products(self):
+        with mock.patch('usgs.EspaWrapper.espa_get') as mockGet:
+            mockGet.return_value = {
+                'olitirs8': {
+                    'products': ['ab', 'sr']
+                },
+                'blah': {
+                    'products': ['blah']
+                },
+                'foo': {
+                    'products': ['sr']
+                }
+            }
+
+            results = EspaWrapper.available_products('aaaaa', 'sr')
+            mockGet.assert_called_once_with('available-products', 'aaaaa')
+            assert results == [
+                ['olitirs8', 'aaaaa', 'sr'],
+                ['foo', 'aaaaa', 'sr']
+            ]
+
+    def test_order_all(self):
+        with mock.patch('usgs.EspaWrapper.available_products') as mockFind, \
+                mock.patch('usgs.EspaWrapper.order') as mockOrder:
+
+            mockFind.return_value = [
+                ['foo', 'aaaaa', 'sr'],
+                ['olitirs8', 'aaaaa', 'sr'],
+            ]
+            mockOrder.side_effect = ['order1', 'order2']
+
+            order_ids = EspaWrapper.order_all('LC08', 'sr')
+            mockFind.assert_called_once_with('LC08', 'sr')
+
+            assert mockOrder.call_count == 2
+            mockOrder.assert_called_with('olitirs8', 'aaaaa', 'sr')
+            assert order_ids == ['order1', 'order2']
+
+    def test_download_urls(self):
+        with mock.patch('usgs.EspaWrapper.espa_get') as mockGet:
+            mockGet.return_value = {
+                'order1234': [
+                    {'product_dload_url': 'url1'},
+                    {'product_dload_url': 'url2'}
+                ]
+            }
+
+            urls = EspaWrapper.download_urls('order1234')
+            mockGet.assert_called_once_with('item-status', 'order1234')
+            assert urls == ['url1', 'url2']
---
+++ theia/usgs/espa_wrapper.py
@@ -0,0 +1,78 @@
+from urllib.parse import urljoin
+from requests.auth import HTTPBasicAuth
+import json
+import requests
+from os import environ
+
+
+class EspaWrapper:
+    @classmethod
+    def api_url(cls, path):
+        # return urljoin('https://demo1580318.mockable.io/', path)
+        return urljoin('https://espa.cr.usgs.gov/api/v1/', path)
+
+    @classmethod
+    def list_orders(cls):
+        return cls.espa_get('list-orders', None)
+
+    @classmethod
+    def available_products(cls, scene_id, desired_product_id):
+        results = cls.espa_get('available-products', scene_id)
+        return [[key, scene_id, desired_product_id] for key in results if desired_product_id in set(results[key]['products'])]
+
+    @classmethod
+    def order_status(cls, order_id):
+        return cls.espa_get('order-status', order_id)['status']
+
+    @classmethod
+    def order(cls, collection, scene_id, product_type):
+        return cls.espa_post('order', {
+            collection: {
+                'inputs': [scene_id],
+                'products': [product_type]
+            },
+            'format': 'gtiff'
+        })['orderid']
+
+    @classmethod
+    def order_all(cls, scene_id, product_id):
+        return [cls.order(*item) for item in cls.available_products(scene_id, product_id)]
+
+    @classmethod
+    def download_urls(cls, order_id):
+        result = cls.espa_get('item-status', order_id)
+        return [item['product_dload_url'] for item in result[order_id]]
+
+    @classmethod
+    def espa_get(cls, url, request_data, **kwargs):
+        new_args = cls.espa_prepare(request_data, **kwargs)
+        new_url = cls.api_url(url)
+        if request_data:
+            new_url = urljoin(new_url+'/', request_data)
+        return requests.get(new_url, **new_args).json()
+
+    @classmethod
+    def espa_post(cls, url, request_data, **kwargs):
+        new_args = cls.espa_prepare(request_data, **kwargs)
+        new_url = cls.api_url(url)
+        if request_data:
+            new_args = {**new_args, **{'json': request_data}}
+        return requests.post(new_url, **new_args).json()
+
+    @classmethod
+    def espa_prepare(cls, request_data, **kwargs):
+        headers = {'Content-Type': 'application/json'}
+        if 'headers' in kwargs:
+            headers = {**headers, **(kwargs['headers'])}
+
+        auth = cls.espa_credentials()
+
+        new_args = {
+            'headers': headers,
+            'auth': auth
+        }
+        return {**kwargs, **new_args}
+
+    @classmethod
+    def espa_credentials(cls, username=environ['USGS_USERNAME'], password=environ['USGS_PASSWORD']):
+        return HTTPBasicAuth(username, password)
