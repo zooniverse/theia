02a7520ab7cb1cbd1c72ebce7287049b72340ce2
Amy Boyer <amy@zooniverse.org>
2019-04-22 10:15:13 -0700

    order first matching scene

--- theia/tasks.py
+++ theia/tasks.py
@@ -1,14 +1,15 @@
 from __future__ import absolute_import
 from celery import shared_task
 import theia.api.models as models
-from theia.usgs import ErosWrapper
+from theia.usgs import ErosWrapper, EspaWrapper
 
 
 @shared_task(name='theia.locate_scenes')
 def locate_scenes(imagery_request_id):
     request = models.ImageryRequest.objects.get(pk=imagery_request_id)
     search = models.ImagerySearch.build_search(request)
-    return ErosWrapper.search(search)
+    scenes = ErosWrapper.search(search)
+    return EspaWrapper.order_all(scenes[0], 'sr')
 
 
 tasks = {
