35ed54d2d117bbf2e6d93bff4dddaa67b6e53a9d
Amy Boyer <amy@zooniverse.org>
2019-04-22 18:12:39 -0700

    ready to go into processing pipeline

--- theia/tasks.py
+++ theia/tasks.py
@@ -22,21 +22,30 @@ def locate_scenes(imagery_request_id):
 @shared_task(name='theia.tasks.wait_for_scene')
 def wait_for_scene(requested_scene_id):
     request = models.RequestedScene.objects.get(pk=requested_scene_id)
+    if request.status == 1:
+        return
+
     status = EspaWrapper.order_status(request.scene_order_id)
 
-    request.checked_at = make_aware(datetime.utcnow())
-
-    if status=='completed':
+    if status=='complete':
         request.status=1
-        print('all done now')
-        # schedule download worker
+        request.scene_url = EspaWrapper.download_urls(request.scene_order_id)[0]
+        process_scene.delay(requested_scene_id)
     else:
-        soon = datetime.utcnow() + timedelta(minutes=5)
+        soon = datetime.utcnow() + timedelta(minutes=15)
         wait_for_scene.apply_async((requested_scene_id,), eta=soon)
 
+    request.checked_at = make_aware(datetime.utcnow())
     request.save()
+
+
+@shared_task(name='theia.tasks.process_scene')
+def process_scene(requested_scene_id):
+    request = models.RequestedScene.objects.get(pk=requested_scene_id)
+    return
 
 tasks = {
     'locate_scenes': locate_scenes,
-    'wait_for_scene': wait_for_scene
+    'wait_for_scene': wait_for_scene,
+    'process_scene': process_scene
 }
