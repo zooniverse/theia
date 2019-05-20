33aedecbc9d8e641d23b284ad88277fc7962e229
Amy Boyer <amy@zooniverse.org>
2019-05-20 14:26:24 -0500

    imagery request tests

--- tests/api/models/test_imagery_request.py
+++ tests/api/models/test_imagery_request.py
@@ -1,11 +1,17 @@
+import pytest
+from unittest.mock import patch
+
 from django.test import TestCase
 from theia.api.models import ImageryRequest
 from datetime import datetime
+
+import theia.tasks
 
 
 class TestImageryRequest(TestCase):
     def setUp(self):
         self.test_request = ImageryRequest(
+            id=3,
             dataset_name='LANDSAT_8_C1',
             project_id=123,
             created_at=datetime.fromisoformat('2019-05-17')
@@ -16,5 +22,10 @@ class TestImageryRequest(TestCase):
 
     def test_post_create(self):
         '''it enqueues a job after being created'''
-        # TODO: write this test soon
-        assert True
+        with patch.object(theia.tasks, 'locate_scenes', autospec=True) as mockLocate:
+            with patch.object(mockLocate.return_value, 'delay', autospec=True) as mockDelay:
+                ImageryRequest.post_save(None, self.test_request, False)
+                mockDelay.assert_not_called()
+
+                ImageryRequest.post_save(None, self.test_request, True)
+                mockDelay.assert_called_once_with(self.test_request.id)
--- theia/api/models/imagery_request.py
+++ theia/api/models/imagery_request.py
@@ -1,9 +1,11 @@
 from django.db import models as models
 from django.db.models.signals import post_save
-from theia.tasks import locate_scenes
+# from theia.tasks import locate_scenes
+import theia.tasks
 from .project import Project
 from .pipeline import Pipeline
 
+import pdb
 
 class ImageryRequest(models.Model):
     adapter_name = models.CharField(max_length=64, null=False)
@@ -37,7 +39,7 @@ class ImageryRequest(models.Model):
     def post_save(cls, sender, instance, created, *args, **kwargs):
         if created:
             # queue up a worker to search for matching scenes
-            locate_scenes.delay(instance.id)
+            theia.tasks.locate_scenes.delay(instance.id)
 
 
 post_save.connect(ImageryRequest.post_save, sender=ImageryRequest)
