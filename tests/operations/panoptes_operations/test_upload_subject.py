e1aab86ed1fc87c6f1bb42a4ba441be8a58602e5
Amy Boyer <amy@zooniverse.org>
2019-05-22 15:12:12 -0500

    even more tests

---
+++ tests/operations/panoptes_operations/test_upload_subject.py
@@ -0,0 +1,125 @@
+import pytest
+from unittest.mock import patch, Mock, PropertyMock
+
+from theia.api.models import JobBundle, Pipeline, Project
+from theia.operations.panoptes_operations import UploadSubject
+from panoptes_client import Subject, SubjectSet
+
+# some of the tests here are weird because writing tests for the python api client
+# is really difficult
+# the panoptes api client intercepts property accesses and so is hard to mock
+class TestUploadSubject:
+    @patch('theia.api.models.Pipeline.name_subject_set', return_value='pipeline name')
+    @patch('theia.operations.panoptes_operations.UploadSubject._connect')
+    @patch('theia.operations.panoptes_operations.UploadSubject._get_subject_set', return_value=SubjectSet())
+    @patch('theia.operations.panoptes_operations.UploadSubject._create_subject', return_value=Subject())
+    @patch('panoptes_client.SubjectSet.add')
+    def test_apply_single(self, mockAdd, mockCreate, mockGet, mockConnect, mockGetName, *args):
+        project = Project(id=8)
+        pipeline = Pipeline(project=project)
+        bundle = JobBundle(pipeline=pipeline)
+        UploadSubject.apply('some_file', bundle)
+
+        mockConnect.assert_called_once()
+        mockGetName.assert_called_once()
+        mockGet.assert_called_once_with(pipeline, 8, 'pipeline name')
+        mockCreate.assert_called_once_with(8, 'some_file')
+        mockAdd.assert_called_once_with(mockCreate.return_value)
+
+    @patch('theia.api.models.JobBundle.name_subject_set', return_value='bundle name')
+    @patch('theia.operations.panoptes_operations.UploadSubject._connect')
+    @patch('theia.operations.panoptes_operations.UploadSubject._get_subject_set', return_value=SubjectSet())
+    @patch('theia.operations.panoptes_operations.UploadSubject._create_subject', return_value=Subject())
+    @patch('panoptes_client.SubjectSet.add')
+    def test_apply_multiple(self, mockAdd, mockCreate, mockGet, mockConnect, mockGetName, *args):
+        project = Project(id=8)
+        pipeline = Pipeline(project=project, multiple_subject_sets=True)
+        bundle = JobBundle(pipeline=pipeline)
+        UploadSubject.apply('some_file', bundle)
+
+        mockConnect.assert_called_once()
+        mockGetName.assert_called_once()
+        mockGet.assert_called_once_with(bundle, 8, 'bundle name')
+        mockCreate.assert_called_once_with(8, 'some_file')
+        mockAdd.assert_called_once_with(mockCreate.return_value)
+
+    @pytest.mark.focus
+    @patch('panoptes_client.Panoptes.connect')
+    @patch('theia.operations.panoptes_operations.PanoptesUtils.url', return_value='sample url')
+    @patch('theia.operations.panoptes_operations.PanoptesUtils.client_id', return_value='sample id')
+    @patch('theia.operations.panoptes_operations.PanoptesUtils.client_secret', return_value='sample secret')
+    def test__connect(self, mockSecret, mockId, mockUrl, mockConnect):
+        UploadSubject._connect()
+        mockUrl.assert_called_once()
+        mockId.assert_called_once()
+        mockSecret.assert_called_once()
+        mockConnect.assert_called_once_with(endpoint='sample url', client_id='sample id', client_secret='sample secret')
+
+    @patch('theia.api.models.JobBundle.save')
+    @patch('theia.api.models.Pipeline.save')
+    @patch('theia.operations.panoptes_operations.UploadSubject._create_subject_set')
+    @patch('panoptes_client.SubjectSet.find', autospec=True)
+    def test__get_subject_set(self, mockFind, mockCreateSet, *args):
+        mockFind.reset_mock()
+        mockCreateSet.reset_mock()
+
+        emptyJobBundle = JobBundle()
+        linkedJobBundle = JobBundle(subject_set_id=3)
+        emptyPipeline = Pipeline()
+        linkedPipeline = Pipeline(subject_set_id=3)
+
+        result = UploadSubject._get_subject_set(emptyJobBundle, 8, 'some name')
+        mockFind.assert_not_called()
+        mockCreateSet.assert_called_once_with(8, 'some name')
+
+        mockFind.reset_mock()
+        mockCreateSet.reset_mock()
+
+        result = UploadSubject._get_subject_set(linkedJobBundle, 8, 'some name')
+        mockFind.assert_called_once_with(3)
+        mockCreateSet.assert_not_called()
+
+        mockFind.reset_mock()
+        mockCreateSet.reset_mock()
+
+        result = UploadSubject._get_subject_set(emptyPipeline, 8, 'some name')
+        mockFind.assert_not_called()
+        mockCreateSet.assert_called_once_with(8, 'some name')
+
+        mockFind.reset_mock()
+        mockCreateSet.reset_mock()
+
+        result = UploadSubject._get_subject_set(linkedPipeline, 8, 'some name')
+        mockFind.assert_called_once_with(3)
+        mockCreateSet.assert_not_called()
+
+    @patch('panoptes_client.Project.find', return_value=Mock())
+    @patch('panoptes_client.Subject.save', autospec=True)
+    @patch('panoptes_client.Subject.add_location', autospec=True)
+    def test__create_subject_no_metadata(self, mockAdd, mockSave, mockFind):
+        created_subject = UploadSubject._create_subject(1, 'some_file')
+        mockFind.assert_called_once_with(1)
+        mockAdd.assert_called_once_with(created_subject, 'some_file')  # weird
+        mockSave.assert_called_once()
+        assert(mockFind.return_value==created_subject.links.project.id)  # weird
+
+    @patch('panoptes_client.Project.find', return_value=Mock())
+    @patch('panoptes_client.Subject.save', autospec=True)
+    @patch('panoptes_client.Subject.add_location', autospec=True)
+    def test__create_subject_with_metadata(self, mockAdd, mockSave, mockFind):
+        created_subject = UploadSubject._create_subject(1, 'some_file', {'foo': 'bar'})
+        mockFind.assert_called_once_with(1)
+        mockAdd.assert_called_once_with(created_subject, 'some_file')  # weird
+        mockSave.assert_called_once()
+        assert(mockFind.return_value==created_subject.links.project.id)  # weird
+        assert(created_subject.metadata=={'foo': 'bar'})
+
+    @patch('panoptes_client.Project.find', return_value=Mock())
+    @patch('panoptes_client.SubjectSet.save', autospec=True)
+    def test__create_subject_set(self, mockSave, mockFind):
+        created_set = UploadSubject._create_subject_set(1, 'some name')
+
+        mockFind.assert_called_once_with(1)
+        mockSave.assert_called_once()
+        assert(created_set.display_name=='some name')
+        assert(mockFind.return_value==created_set.links.project.id)  # weird
