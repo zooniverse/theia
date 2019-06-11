38e52b27c5c2132219c91c2310cdcc82a3960424
Amy Boyer <amy@zooniverse.org>
2019-06-11 14:44:25 -0500

    clean up messy test

--- tests/operations/image_operations/test_remap_image.py
+++ tests/operations/image_operations/test_remap_image.py
@@ -1,23 +1,10 @@
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 import numpy as np
 
 from theia.operations.image_operations import RemapImage
 from theia.api.models import ImageryRequest, JobBundle, PipelineStage
-
-
-class MockTIFF:
-    def read_image(self):
-        pass
-
-
-class MockImage:
-    def convert(self, toType):
-        pass
-
-    def save(self, filename):
-        pass
 
 
 class TestRemapImage:
@@ -33,21 +20,18 @@ class TestRemapImage:
 
     @patch('theia.adapters.dummy.Adapter.remap_pixel')
     @patch('theia.utils.FileUtils.version_filename', return_value='versioned filename')
-    @patch('libtiff.TIFF.open', return_value=MockTIFF())
-    @patch('PIL.Image.fromarray', return_value=MockImage())
+    @patch('libtiff.TIFF.open', return_value=Mock())
+    @patch('PIL.Image.fromarray', return_value=Mock())
     def test_do_apply(self, mockFromArray, mockOpen, mockRename, mockRemap):
         mockRemap.return_value = self.dummy_array
-        with patch.object(mockOpen.return_value, 'read_image') as mockRead:
-            with patch.object(mockFromArray.return_value, 'convert') as mockConvert:
-                with patch.object(mockFromArray.return_value, 'save') as mockSave:
-                    mockRead.return_value=self.dummy_array
+        mockOpen.return_value.read_image.return_value = self.dummy_array
 
-                    RemapImage.do_apply('dummy', 'input_filename', 2)
+        RemapImage.do_apply('dummy', 'input_filename', 2)
 
-                    mockRename.assert_called_once_with('input_filename', 2)
-                    mockOpen.assert_called_once_with('input_filename')
-                    mockRead.assert_called_once_with()
-                    mockRemap.assert_called_once_with(self.dummy_array)
-                    mockFromArray.assert_called_once_with(self.dummy_array)
-                    mockConvert.assert_called_once_with('L')
-                    mockSave.assert_called_once_with('versioned filename')
+        mockRename.assert_called_once_with('input_filename', 2)
+        mockOpen.assert_called_once_with('input_filename')
+        mockOpen.return_value.read_image.assert_called_once_with()
+        mockRemap.assert_called_once_with(self.dummy_array)
+        mockFromArray.assert_called_once_with(self.dummy_array)
+        mockFromArray.return_value.convert.assert_called_once_with('L')
+        mockFromArray.return_value.save.assert_called_once_with('versioned filename')
