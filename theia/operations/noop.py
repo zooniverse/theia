cb84aaddee74b175e5a67e87f4312d7b7a354273
Chelsea Troy <chelsea.dommert@gmail.com>
2020-03-03 10:59:12 -0500

    Adjust noop to produce directory indicating its presence in the pipeline, though it does not change the files.

--- theia/operations/noop.py
+++ theia/operations/noop.py
@@ -3,4 +3,5 @@ from .abstract_operation import AbstractOperation
 
 class NoOp(AbstractOperation):
     def apply(self, filenames):
+        self.establish_output_directory()
         print("NOOP")  # pragma:nocover
