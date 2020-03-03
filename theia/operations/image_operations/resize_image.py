73e3eefd3f1eb9eed5ff992a45d577c76442a3e7
Chelsea Troy <chelsea.dommert@gmail.com>
2020-03-03 11:09:34 -0500

    Give resized imges their own name so they can be easily identified

--- theia/operations/image_operations/resize_image.py
+++ theia/operations/image_operations/resize_image.py
@@ -1,13 +1,22 @@
 from PIL import Image
 from ..abstract_operation import AbstractOperation
+import os
 
 
 class ResizeImage(AbstractOperation):
     def apply(self, filenames):
+        self.establish_output_directory()
+
         for filename in filenames:
-            new_filename = self.get_new_version(filename)
-            dimensions = (self.config['width'], self.config['height'],)
+            file_title = os.path.splitext(os.path.basename(filename))[0]
+            dimensions = (self.config['width'], self.config['height'])
+            new_filename = '%s_resized_to_%d_%d.%s' % (
+                file_title,
+                self.config['width'],
+                self.config['height'],
+                self.output_extension
+            )
 
             im = Image.open(filename)
             im.thumbnail(dimensions, Image.ANTIALIAS)
-            im.save(new_filename)
+            im.save(self.output_directory + "/" + new_filename)
