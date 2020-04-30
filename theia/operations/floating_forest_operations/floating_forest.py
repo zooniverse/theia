cec472424ac352d936f6816e13a252cb126e6fc1
Chelsea Troy <chelsea.dommert@gmail.com>
2020-04-30 13:51:56 -0500

    Set up operation files for the custom task

---
+++ theia/operations/floating_forest_operations/floating_forest.py
@@ -0,0 +1,25 @@
+import csv
+from PIL import Image
+
+from ..abstract_operation import AbstractOperation
+# https://docs.python.org/3/library/csv.html
+
+
+class FloatingForest(AbstractOperation):
+    def apply(self, filenames):
+        self.establish_output_directory()
+
+        output_filename = self.get_new_version(f"{self.config['filename']}.{self.output_extension}")
+
+        try:
+            channels = []
+
+            for name in filenames:
+                channels.append(Image.open(name).convert('L'))
+
+            merged = Image.merge('RGB', tuple(channels))
+
+            merged.save(self.output_directory + "/" + output_filename)
+
+        except ValueError as e:
+            raise ValueError("e.message " + str(filenames))
