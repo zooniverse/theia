0ed2db3878226bc2a023b16ead19c1f9c99eb3cb
yuenmichelle1 <michelle@zooniverse.org>
2022-03-29 16:19:39 -0700

    update to work on windows

--- start_server.sh
+++ start_server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
-if [ ! "$DJANGO_ENV" == "production" ] && ! [ "$DJANGO_ENV" == "staging" ] ; then
+if [ ! "$DJANGO_ENV" == "production" ] && ! [ "$DJANGO_ENV" == "staging" ]
+then
   while ! nc -z postgres 5432; do
     echo Waiting for Postgres
     sleep 3
   done
 fi
-
 echo Applying migrations
 python manage.py migrate --noinput
-
-if [ "$DJANGO_ENV" == "production" ]; then
-  if [ ! -d "theia/static" ]; then
+if [ "$DJANGO_ENV" == "production" ]
+then
+  if [ ! -d "theia/static" ]
+  then
     echo Generating static files
     python manage.py collectstatic
   fi
--- start_worker.sh
+++ start_worker.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 echo Adding tmp dir
 mkdir tmp
-
 echo Removing logs
 rm tmp/*.log
-
 echo Starting Celery
 exec celery -A theia worker -l info
