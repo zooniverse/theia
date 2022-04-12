8287cbf2189979597d560dd3a86f226a55ae515d
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-04-12 13:39:48 -0500

    WIP grabbing rejected tiles from tmp

--- theia/operations/panoptes_operations/upload_subject.py
+++ theia/operations/panoptes_operations/upload_subject.py
@@ -1,4 +1,4 @@
-from os import getenv, path, path
+from os import getenv, path, listdir
 import csv
 
 from ..abstract_operation import AbstractOperation
@@ -10,7 +10,7 @@ from PIL import Image
 from datetime import datetime
 
 class UploadSubject(AbstractOperation):
-    def apply(self, filenames):
+    def apply(self, accepted_filenames):
         if self.pipeline.multiple_subject_sets:
             scope = self.bundle
         else:
@@ -21,15 +21,18 @@ class UploadSubject(AbstractOperation):
             client_id=PanoptesUtils.client_id(),
             client_secret=PanoptesUtils.client_secret()
         )
+        print('MDY114 TYPE ACCEPTED')
+        print(type(accepted_filenames))
+        print(accepted_filenames)
 
         with theia_authenticated_client:
             target_set = self._get_subject_set(scope, self.project.id, scope.name_subject_set())
-
             using_manifest = False
             metadata_dictionary = {}
 
-            path_example = filenames[0]
+            path_example = accepted_filenames[0]
             manifest_file_location = path.join((path.dirname(path_example) + "_interstitial_products"), "manifest.csv")
+
             if self.include_metadata and path.exists(manifest_file_location):
                 using_manifest = True
                 with open(manifest_file_location, newline='') as csvfile:
@@ -37,19 +40,41 @@ class UploadSubject(AbstractOperation):
                     for row in reader:
                         metadata_dictionary[row['#filename']] = row
 
-            for filename in filenames:
-                img = Image.open(filename)
-                img.save(filename, 'png')
+            for accepted_filename in accepted_filenames:
+                img = Image.open(accepted_filename)
+                img.save(accepted_filename, 'png')
 
                 #This line might have to be done with os.path to translate across OSes
-                name_only = filename.split("/")[len(filename.split("/")) - 1]
+                name_only = accepted_filename.split("/")[len(accepted_filename.split("/")) - 1]
 
                 metadata = {}
                 if using_manifest:
                     metadata = metadata_dictionary[name_only]
 
-                new_subject = self._create_subject(self.project.id, filename, metadata=metadata)
+                new_subject = self._create_subject(self.project.id, accepted_filename, metadata=metadata)
                 target_set.add(new_subject)
+            
+            ##### TODO: DELETE THIS LATER. ONLY APPLICABLE FOR FLOATING FORESTS RESEARCH TEAM DEBUGING ##### 
+
+            rejected_tile_location =  path.join((path.dirname(path_example) + "_interstitial_products"), "rejected")
+
+            rejected_tiles = [rejected_file for rejected_file in listdir(rejected_tile_location) if path.isfile(path.join(rejected_tile_location, rejected_file))]
+
+            rejected_subject_set = self._create_subject_set(self.project.id, scope.name_subject_set()+ ' rejected')
+
+            for rejected_filename in rejected_tiles:
+                img = Image.open(rejected_filename)
+                img.save(rejected_filename, 'png')
+
+                name_only = rejected_filename.split("/")[len(rejected_filename.split("/")) - 1]
+
+                metadata = {}
+                if using_manifest:
+                    metadata = metadata_dictionary[name_only]
+
+                new_subject = self._create_subject(self.project.id, rejected_filename, metadata=metadata)
+                rejected_subject_set.add(new_subject)
+            #### TODO: END OF DELETE. DELETE ABOVE FROM LAST TODO #### 
 
 
     def _get_subject_set(self, scope, project_id, set_name):
