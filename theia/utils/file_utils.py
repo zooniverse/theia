f7ec1b0e42e709ab071ee3491b072c3a640e8f83
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-07 20:00:12 +0000

    Adding tarfile member sanitization to extractall()

--- theia/utils/file_utils.py
+++ theia/utils/file_utils.py
@@ -27,7 +27,26 @@ class FileUtils:
             # make the temp directory if it doesn't already exist
             if not os.path.isdir(target):
                 os.mkdir(target)
-            archive.extractall(target)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, target)
 
     @classmethod
     def absolutize(cls, *, bundle=None, work_dir=None, input_dir=None, filename, new_extension=None):
