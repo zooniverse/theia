16ce486f07b9017eacb4013d46dd4e007dbf7343
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-02-12 15:52:08 -0600

    remove unused code from tasks.py wait_for_scene

--- theia/adapters/usgs/tasks.py
+++ theia/adapters/usgs/tasks.py
@@ -1,6 +1,5 @@
 from celery import shared_task
 from theia.api import models
-# from .espa_wrapper import EspaWrapper
 from .eros_wrapper import ErosWrapper
 
 from datetime import datetime, timedelta
@@ -9,30 +8,20 @@ from django.utils.timezone import make_aware
 
 @shared_task(name='theia.adapters.usgs.tasks.wait_for_scene')
 def wait_for_scene(requested_scene_id, available_products):
-    print('MDY114 AVAILABLE PRODUCTS IN WAIT')
-    print(available_products)
     request = models.RequestedScene.objects.get(pk=requested_scene_id)
     if request.status == 1:
         return
 
-    # TO DO CHANGES THIS TO REQUEST FROM REQUEST DOWNLOAD
-    #status = EspaWrapper.order_status(request.scene_order_id)
     eros_wrapper = ErosWrapper()
     rerequest_download_result = eros_wrapper.request_download(available_products)
-    print('MDY114 REREQUEST DOWNLOAD')
-    print(rerequest_download_result)
     now = make_aware(datetime.utcnow())
     request.checked_at = now
     request.save()
 
     if rerequest_download_result['preparingDownloads'] != None and len(rerequest_download_result['preparingDownloads']) == 0:
         request.status = 1
-        print('MDY114 BEFORE AVAIL DOWNLOADS CHECK')
-        print(rerequest_download_result['availableDownloads'])
-        print(len(rerequest_download_result['availableDownloads']))
         if len(rerequest_download_result['availableDownloads']) > 0:
             for item in rerequest_download_result['availableDownloads']:
-                print(item)
                 request.scene_url = item['url']
         request.save()
         models.JobBundle.objects.from_requested_scene(request)
