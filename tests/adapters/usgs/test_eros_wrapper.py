346247a59cb5477663e6f4e2ac5cb4822ea5a7a1
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-02-15 14:25:07 -0600

    add tests for requesting available products

--- tests/adapters/usgs/test_eros_wrapper.py
+++ tests/adapters/usgs/test_eros_wrapper.py
@@ -2,9 +2,9 @@ from theia.adapters.usgs import ErosWrapper
 from unittest.mock import patch, call
 import datetime
 THREE_HOURS_AGO = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
+FAKE_SEARCH = {'datasetName' : 'LANDSAT_BAND'}
 
 class TestErosWrapper:
-
     def test_is_token_expired_no_login_time(self):
         is_expired = ErosWrapper().is_token_expired()
         assert is_expired == True
@@ -56,8 +56,7 @@ class TestErosWrapper:
     @patch('theia.adapters.usgs.Utils.get_password', return_value='feynmansSuperSecurePassowrd')
     @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value={'results': ['LC01_FAKESCENE_007']})
     def test_send_search_request(self, mockRequestSend, *_):
-        fakeSearch = {'datasetName' : 'LANDSAT_BAND'}
-        ErosWrapper().search(search=fakeSearch)
+        ErosWrapper().search(search=FAKE_SEARCH)
         mockRequestSend.assert_has_calls([
             call(
                 'https://m2m.cr.usgs.gov/api/api/json/stable/login',
@@ -103,10 +102,9 @@ class TestErosWrapper:
 
     @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=200)
     def test_add_scene_to_order_list(self, mock_send_request):
-        fakeSearch = {'datasetName' : 'LANDSAT_BAND'}
         eros_wrapper = ErosWrapper()
         eros_wrapper.login_time = datetime.datetime.utcnow()
-        eros_wrapper.add_scenes_to_order_list(scene_id=1, search=fakeSearch)
+        eros_wrapper.add_scenes_to_order_list(scene_id=1, search=FAKE_SEARCH)
         mock_send_request.assert_has_calls([
             call(
                 'https://m2m.cr.usgs.gov/api/api/json/stable/scene-list-add',
@@ -118,9 +116,54 @@ class TestErosWrapper:
     @patch('theia.adapters.usgs.ErosWrapper.login')
     @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=200)
     def test_add_scene_to_order_list_logs_in(self, _, mock_login):
-        fakeSearch = {'datasetName' : 'LANDSAT_BAND'}
         eros_wrapper = ErosWrapper()
-        eros_wrapper.add_scenes_to_order_list(scene_id=1, search=fakeSearch)
+        eros_wrapper.add_scenes_to_order_list(scene_id=1, search=FAKE_SEARCH)
         mock_login.assert_called_once()
 
+    @patch('theia.adapters.usgs.ErosWrapper.login')
+    @patch('theia.adapters.usgs.ErosWrapper.send_request')
+    def test_available_products_logs_in(self, _, mock_login):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.available_products(list_id=1, search=FAKE_SEARCH)
+        mock_login.assert_called_once()
+
+    @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=[{'bulkAvailable': True, 'downloadSystem': 'dds', 'entityId': 'LC01_FAKESCENE_007', 'displayId': 'LC01FAKESCENE007', 'id': 1}])
+    def test_available_products(self, mock_send_request):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = datetime.datetime.utcnow()
+        avail_products = eros_wrapper.available_products(list_id=1, search=FAKE_SEARCH)
+        mock_send_request.assert_has_calls([
+            call(
+                'https://m2m.cr.usgs.gov/api/api/json/stable/download-options',
+                 {'listId': 1, 'datasetName': 'LANDSAT_BAND'}
+            )
+        ],
+        any_order=False)
+        assert len(avail_products) == 1
+        assert avail_products[0] == {
+            'entityId': 'LC01_FAKESCENE_007',
+            'productId': 1,
+            'displayId': 'LC01FAKESCENE007'
+        }
+
+    @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=[])
+    def test_available_products_has_no_results(self, _):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = datetime.datetime.utcnow()
+        avail_products = eros_wrapper.available_products(list_id=1, search=FAKE_SEARCH)
+        assert avail_products == []
+
+    @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=[{'bulkAvailable': False, 'downloadSystem': 'dds', 'entityId': 'LC01_FAKESCENE_007', 'displayId': 'LC01FAKESCENE007', 'id': 1}])
+    def test_avail_products_result_not_available(self, _):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = datetime.datetime.utcnow()
+        avail_products = eros_wrapper.available_products(list_id=1, search=FAKE_SEARCH)
+        assert avail_products == []
+
+    @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=[{'bulkAvailable': True, 'downloadSystem': 'folder', 'entityId': 'LC01_FAKESCENE_007', 'displayId': 'LC01FAKESCENE007', 'id': 1}])
+    def test_avail_products_result_download_system_incorrect(self, _):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = datetime.datetime.utcnow()
+        avail_products = eros_wrapper.available_products(list_id=1, search=FAKE_SEARCH)
+        assert avail_products == []
 
