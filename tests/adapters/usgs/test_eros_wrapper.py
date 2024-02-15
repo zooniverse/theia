29dd53e67e4dad3c6e93154a6c1dde373cfb3952
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2024-02-15 14:01:53 -0600

    add login and token expiration check tests

--- tests/adapters/usgs/test_eros_wrapper.py
+++ tests/adapters/usgs/test_eros_wrapper.py
@@ -1,9 +1,56 @@
 from theia.adapters.usgs import ErosWrapper
-
 from unittest.mock import patch, call
 import datetime
+THREE_HOURS_AGO = datetime.datetime.utcnow() - datetime.timedelta(hours=3)
 
 class TestErosWrapper:
+
+    def test_is_token_expired_no_login_time(self):
+        is_expired = ErosWrapper().is_token_expired()
+        assert is_expired == True
+
+    def test_is_token_expired_login_time_past_expiry(self):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = THREE_HOURS_AGO
+        assert eros_wrapper.is_token_expired() == True
+
+    def test_is_token_expired_login_time_not_expired(self):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = datetime.datetime.utcnow()
+        assert eros_wrapper.is_token_expired() == False
+
+    @patch('theia.adapters.usgs.Utils.get_username', return_value='Richard Feynman')
+    @patch('theia.adapters.usgs.Utils.get_password', return_value='feynmansSuperSecurePassowrd')
+    @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=200)
+    def test_login_no_login_time(self, mock_send_request, *_):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login()
+        mock_send_request.assert_has_calls([
+            call(
+                'https://m2m.cr.usgs.gov/api/api/json/stable/login',
+                 {'username': 'Richard Feynman', 'password': 'feynmansSuperSecurePassowrd'}
+                 )
+        ])
+
+    @patch('theia.adapters.usgs.Utils.get_username', return_value='Richard Feynman')
+    @patch('theia.adapters.usgs.Utils.get_password', return_value='feynmansSuperSecurePassowrd')
+    @patch('theia.adapters.usgs.ErosWrapper.send_request', return_value=200)
+    def test_login_token_expired(self, mock_send_request, *_):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login()
+        mock_send_request.assert_has_calls([
+            call(
+                'https://m2m.cr.usgs.gov/api/api/json/stable/login',
+                 {'username': 'Richard Feynman', 'password': 'feynmansSuperSecurePassowrd'}
+                 )
+        ])
+
+    @patch('theia.adapters.usgs.ErosWrapper.send_request')
+    def test_login_not_called_when_token_not_expired(self, mock_send_request):
+        eros_wrapper = ErosWrapper()
+        eros_wrapper.login_time = datetime.datetime.utcnow()
+        eros_wrapper.login()
+        mock_send_request.assert_not_called()
 
     @patch('theia.adapters.usgs.Utils.get_username', return_value='Richard Feynman')
     @patch('theia.adapters.usgs.Utils.get_password', return_value='feynmansSuperSecurePassowrd')
